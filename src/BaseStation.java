a3044ff1248628780a4c07528175eb549444230d
Jeffrey Hogan <jeff.hogan1@gmail.com>
2014-07-13 10:23:21 -0500

    adding some communication libraries we may end up using

--- src/BaseStation.java
+++ src/BaseStation.java
@@ -1,5 +1,5 @@
 
-public class HASystem {
+public class BaseStation {
 
 	String GetStatus() {
 		
--- src/Driver.java
+++ src/Driver.java
@@ -2,8 +2,8 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		HASystem haSystem = new HASystem();
-		System.out.println(haSystem.GetStatus());
+		BaseStation baseStation = new BaseStation();
+		System.out.println(baseStation.GetStatus());
 
 	}
 
