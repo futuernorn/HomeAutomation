8d2844aa14b46cd3527bcba7b16538eeec1fbcec
zyphet <zyphet@gmail.com>
2014-07-23 19:02:30 -0500

    Changed Testfiles

--- src/org/cs4398_G4/testcases/BehaviorTest.java
+++ src/org/cs4398_G4/testcases/BehaviorTest.java
@@ -1,37 +1,45 @@
 package org.cs4398_G4.testcases;
 
 import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.cs4398_G4.Action;
+import org.cs4398_G4.Behavior;
+import org.cs4398_G4.Condition;
+import org.cs4398_G4.Sensor;
 import org.junit.Test;
+
+import com.pi4j.io.gpio.GpioPin;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 
 public class BehaviorTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		System.out.println("@BeforeClass - oneTimeSetUp");
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		System.out.println("@AfterClass - oneTimeTearDown");
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
-	public void test() {
-//		Behavior tester = new Behavior();
+	public void testHandleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
 		
+		GpioPin testEventPin = event.getPin();
+		PinState testEventState = event.getState();
 		
+		List<Condition> testConditions = new ArrayList<Condition>();
+		List<Action> testActions = new ArrayList<Action>();
+		
+		Sensor testSensor = new Sensor("testSensorString", null, null);
+		
+		Condition testCondition = new Condition(testSensor, testEventState, 1);
+		
+		Behavior tester = new Behavior("TestBehavior", testConditions, testActions);
+		
+		//assertEquals("", null, testConditions.getElapsedTime());
+	}
+	
+	@Test
+	private void Run() {
+		fail("Not yet implemented");
 	}
 
 }
---
+++ src/org/cs4398_G4/testcases/ConditionTest.java
@@ -0,0 +1,20 @@
+package org.cs4398_G4.testcases;
+
+import static org.junit.Assert.*;
+
+import org.cs4398_G4.Behavior;
+import org.junit.Test;
+
+public class ConditionTest {
+
+	@Test
+	public void startTimer(final Behavior behavior) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void stopTimer() {
+		fail("Not yet implemented");
+	}
+
+}
