046ef8b4d161110492cbe138b11aa2496f5bb8fe
zyphet <zyphet@gmail.com>
2014-07-24 18:20:56 -0500

    Room test
    Tests to see if a light and sensor is properly added after new room is
    created.

--- src/org/cs4398_G4/testcases/RoomTest.java
+++ src/org/cs4398_G4/testcases/RoomTest.java
@@ -2,9 +2,11 @@ package org.cs4398_G4.testcases;
 
 import static org.junit.Assert.*;
 
-
 import java.util.HashMap;
 
+import org.cs4398_G4.Actuator;
+import org.cs4398_G4.Room;
+import org.cs4398_G4.Sensor;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -42,26 +44,35 @@ public class RoomTest {
 	public void AddComponentTest() {
 		
 		//Room tester object
-//		Room RoomTester = new Room();
+		Room RoomTester = new Room("testRoom");
 		
-		//Test Pins
+		assertEquals("Room should have no actuators when created", 0, RoomTester.GetActuators().size());
+		assertEquals("Room should have no sensors when created", 0, RoomTester.GetSensors().size());
+		
+		//Create Hashmap for pins
 		HashMap<String, Pin> inputPinNumbers = new HashMap<String, Pin>();
-		inputPinNumbers.put("SensorInput", RaspiPin.GPIO_00);
-		
 		HashMap<String, Pin> outputPinNumbers = new HashMap<String, Pin>();
+		
+		//put pins for sensor and light
+		inputPinNumbers.put("SensorInput", RaspiPin.GPIO_00);
 		outputPinNumbers.put("SensorOutput", RaspiPin.GPIO_01);
 		
 		inputPinNumbers.put("LightInput", RaspiPin.GPIO_03);
-		
 		outputPinNumbers.put("LightOutput", RaspiPin.GPIO_04);
 		
-		//Test Sensor Component
-//		Sensor testSensor = new Sensor(inputPinNumbers, outputPinNumbers, "testSensor");
-//		RoomTester.AddComponent(testSensor);
-//		
-//		//Test Light Component
-//		Actuator testLight = new Actuator(inputPinNumbers, outputPinNumbers);
-//		RoomTester.AddComponent(testLight);
+		//create a sensor
+		Sensor testSensor = new Sensor("test Sensor", inputPinNumbers, outputPinNumbers);
+		
+		//create light
+		Actuator testLight = new Actuator("test light", inputPinNumbers, outputPinNumbers);
+		
+		//Add sensor and light to room
+		RoomTester.AddComponent(testLight);
+		RoomTester.AddComponent(testSensor);
+		
+		//check if room contains one light and one sensor
+		assertEquals("Room should contain one sensor", 1, RoomTester.GetSensors().size());
+		assertEquals("Room should contain one light", 1, RoomTester.GetActuators().size());
 		
 		
 	}
